591543b9b6d97e2836d3e09e72be15d1fab6dbe3
dysanchez <dylan.sr2002@gmail.com>
2025-03-24 02:12:19 -0500

    Agregar class cursor-pointer

--- src/app/components/searchBar.tsx
+++ src/app/components/searchBar.tsx
@@ -34,7 +34,9 @@ export default function SearchBar() {
     >
       <h1
         className={`text-gradient font-black shadow-text drop-shadow-md border-blue-500 p-2 duration-300 ${
-          !hasSearched ? "text-2xl sm:text-7xl w-full mb-20" : "text-xl sm:text-4xl w-full sm:w-2/6"
+          !hasSearched
+            ? "text-2xl sm:text-7xl w-full mb-20"
+            : "text-xl sm:text-4xl w-full sm:w-2/6"
         }`}
       >
         SkyConnect Explorer
@@ -50,7 +52,7 @@ export default function SearchBar() {
       />
       <button
         type="submit"
-        className={`bg-gradient-to-r from-cyan-500 to-blue-500 text-white px-4 py-2 rounded transition-all duration-200 flex items-center justify-center gap-2 w-40`}
+        className={`cursor-pointer bg-gradient-to-r from-cyan-500 to-blue-500 text-white px-4 py-2 rounded transition-all duration-200 flex items-center justify-center gap-2 w-40`}
       >
         <Image
           src="/find.png"
